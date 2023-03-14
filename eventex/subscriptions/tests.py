ccf7d28a7c278669f12e850ab3c71c9df45cbda6
kevinidias <kevin.dias.20160@gmail.com>
2023-03-14 08:23:08 -0300

    criando view para enviar email e testes

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core import mail
 from eventex.subscriptions.forms import SubscriptionForm
 
 
@@ -34,3 +35,45 @@ class SubscribeTest(TestCase):
         """Form must have 4 fields."""
         form = self.resp.context['form']
         self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Kevin', cpf='12345678901', email='kevin.dias.20160@gmail.com', phone='24-00000-000')
+
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        """Valid POST should redirect to /inscricao/"""
+        self.assertEqual(302, self.resp.status_code)
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, email.subject)
+
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, email.from_email)
+
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expect = ['contato@eventex.com.br', 'kevin.dias.20160@gmail.com']
+
+        self.assertEqual(expect, email.to)
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+
+        self.assertIn('Kevin', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('kevin.dias.20160@gmail.com', email.body)
+        self.assertIn('24-00000-000', email.body)
+
+
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.core import mail
+from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    context = {'form': SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        form.full_clean()
 
+        context = dict(name='Kevin', cpf='12345678901', email='kevin.dias.20160@gmail.com', phone='24-00000-000')
+
+        body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
+
+        mail.send_mail('Confirmação de inscrição', 
+                       body,
+                       'contato@eventex.com.br',
+                       ['contato@eventex.com.br', form.cleaned_data['email']])
+        
+        return HttpResponseRedirect('/inscricao/')      
+    
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
+    
